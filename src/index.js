e3efbd99d465a90b8f3f783a41faa44abc2b5a46
Gordon <geniusgordon@gmail.com>
2016-05-26 19:28:34 +0800

    react-router-redux actionCreator

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { browserHistory } from 'react-router';
+import { syncHistoryWithStore } from 'react-router-redux';
 import Root from './Root';
 import configureStore from './store';
 
-const store = configureStore();
+const store = configureStore(browserHistory);
+const history = syncHistoryWithStore(browserHistory, store);
 
 render(
-  <Root store={store} history={browserHistory} />,
+  <Root store={store} history={history} />,
   document.getElementById('root')
 );
 
@@ -15,7 +17,7 @@ if (module.hot) {
   module.hot.accept('./Root', () => {
     const NextRoot = require('./Root').default; // eslint-disable-line global-require
     render(
-      <NextRoot store={store} history={browserHistory} />,
+      <NextRoot store={store} history={history} />,
       document.getElementById('root')
     );
   });
