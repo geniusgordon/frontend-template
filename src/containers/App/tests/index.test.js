6cf55479ea08ade08fce9133afee7e0e5d3e6d60
Gordon <geniusgordon@gmail.com>
2016-05-29 18:42:44 +0800

    css-modules-require-hook

--- src/containers/App/tests/index.test.js
+++ src/containers/App/tests/index.test.js
@@ -3,12 +3,13 @@ import React from 'react';
 import { expect } from 'chai';
 import { shallow } from 'enzyme';
 import { App } from '../index';
+import style from '../style.css';
 
 describe('<App />', () => {
   it('should render App title and message', () => {
     const wrapper = shallow(<App message="123" />);
-    expect(wrapper.childAt(0).text()).to.equal('App');
-    expect(wrapper.childAt(1).text()).to.equal('123');
+    expect(wrapper.find(`.${style.appTitle}`).at(0).text()).to.equal('App');
+    expect(wrapper.find(`.${style.appMessage}`).at(0).text()).to.equal('123');
   });
 
   it('should render it\'s children', () => {
