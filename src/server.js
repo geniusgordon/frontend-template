c49be84557e040ab13a9e1897f6ce23fde0bcf85
Gordon <geniusgordon@gmail.com>
2016-06-03 21:34:06 +0800

    server.js

---
+++ src/server.js
@@ -0,0 +1,19 @@
+import path from 'path';
+import { createServer } from 'http';
+import express from 'express';
+import logger from 'morgan';
+
+const app = express();
+const port = 3000;
+
+app.use(logger('dev'));
+app.use('/static', express.static(path.join(__dirname, '../static')));
+app.use('*', (req, res) => {
+  res.sendFile('index.html', {
+    root: path.join(__dirname, '/../'),
+  });
+});
+app.listen(port, () => {
+  console.log(`listening on port: ${port}`);
+});
+
