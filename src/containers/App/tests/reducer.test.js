3f5ed8fa10e76c6893b40dfbde4b98743a9478be
Gordon <geniusgordon@gmail.com>
2016-11-14 15:39:24 -0700

    migrate to jest

--- src/containers/App/tests/reducer.test.js
+++ src/containers/App/tests/reducer.test.js
@@ -1,16 +1,17 @@
-import test from 'ava';
 import reducer from '../reducer';
 import { message, increment } from '../actions';
 
-test('message', (t) => {
-  const state = {};
-  const newState = reducer(state, message('test'));
-  t.is(newState.message, 'test');
-});
+describe('app reducer', () => {
+  it('set new message', () => {
+    const state = {};
+    const newState = reducer(state, message('test'));
+    expect(newState.message).toBe('test');
+  });
 
-test('increment', (t) => {
-  const state = { count: 1 };
-  const newState = reducer(state, increment(1));
-  t.is(newState.count, 2);
+  it('increment counter', () => {
+    const state = { count: 1 };
+    const newState = reducer(state, increment(1));
+    expect(newState.count).toBe(2);
+  });
 });
 
